838792244275854ea779e8a622ab1acaea971c57
LeandroSeverinoSilva09 <160742097+LeandroSeverinoSilva09@users.noreply.github.com>
2024-06-06 20:57:18 -0300

    Adição da lógica de login

--- meu_carro_minha_vida/src/meu_carro_minha_vida/Telaselecao.java
+++ meu_carro_minha_vida/src/meu_carro_minha_vida/Telaselecao.java
@@ -194,6 +194,7 @@ public class TelaSelecao extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
--- meu_carro_minha_vida/src/meu_carro_minha_vida/UserDao.java
+++ meu_carro_minha_vida/src/meu_carro_minha_vida/UserDao.java
@@ -1,6 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package meu_carro_minha_vida;
 
-import java.sql.PreparedStatement;
+/**
+ *
+ * @author leandro
+ */
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
@@ -11,6 +20,20 @@ public class UserDao {
                 + cidade + "', '" + login + "', '" + senha + "');";
         conexao.executarSQL(sql);
         conexao.closeConDB();
+
+    }
+
+    public boolean login(String login, String senha) throws SQLException {
+        String sql = "SELECT login, senha FROM usuarios WHERE login = '" + login + "' and senha = '" + senha + "';";
+        ResultSet resultado = null;
+        resultado = conexao.executarConsulta(sql);
+        if (resultado.next()) {
+            conexao.closeConDB();
+            return true;
+        } else {
+            conexao.closeConDB();
+            return false;
+        }
 
     }
 }
